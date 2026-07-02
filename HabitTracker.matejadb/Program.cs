1295e27d0a496029bd4fad163f4e2b3021fe5cd9
matejadb <92729211+matejadb@users.noreply.github.com>
2026-07-02 23:46:29 +0200

    corrections

--- HabitTracker.matejadb/Program.cs
+++ HabitTracker.matejadb/Program.cs
@@ -6,14 +6,14 @@ namespace HabitTracker.matejadb
     {
         static void Main(string[] args)
         {
-            string connectionString = "Data Source=coding_tracker.db";
+            string connectionString = "Data Source=habit_tracker.db";
 
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
 
                 var command = connection.CreateCommand();
-                command.CommandText = @"CREATE TABLE IF NOT EXISTS coding_tracker (
+                command.CommandText = @"CREATE TABLE IF NOT EXISTS coding (
                   Id INTEGER PRIMARY KEY AUTOINCREMENT, 
                   Date TEXT NOT NULL,
                   Occurance INTEGER NOT NULL)";
