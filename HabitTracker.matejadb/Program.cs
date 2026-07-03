8b7837463b81c1ce0c361d5de48fbd1fee35fc4d
matejadb <92729211+matejadb@users.noreply.github.com>
2026-07-03 20:26:33 +0200

    handle non existent id

--- HabitTracker.matejadb/Program.cs
+++ HabitTracker.matejadb/Program.cs
@@ -55,6 +55,7 @@ class Program
                 break;
             case "2":
                 UpdateHabit();
+                Menu();
                 break;
             case "3":
                 ViewHabits();
@@ -65,6 +66,7 @@ class Program
                 break;
             case "4":
                 DeleteHabit();
+                Menu();
                 break;
             case "5":
                 Console.WriteLine("Exiting the application.");
@@ -76,116 +78,6 @@ class Program
                 Menu();
                 break;
         }
-    }
-
-    // Validate Id
-    private static void DeleteHabit()
-    {
-        ViewHabits();
-        Console.Write("Select an id of the occurance you wish to delete: ");
-        string id = Console.ReadLine();
-
-        try
-        {
-
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-                var command = connection.CreateCommand();
-
-                command.CommandText = $"DELETE FROM drinking_water WHERE Id={id}";
-
-                command.ExecuteNonQuery();
-
-
-                connection.Close();
-            }
-        }
-        catch (SqliteException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        Console.WriteLine("\nOccurance deleted successfully!\nPress any key to continue...");
-        Console.ReadKey();
-
-        Menu();
-    }
-
-    private static void ViewHabits()
-    {
-        Console.Clear();
-        Console.WriteLine("======WATER DRINKING HISTORY======");
-        Console.WriteLine("\nId\tDate\t\tOccurance\n");
-        try
-        {
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-                var command = connection.CreateCommand();
-
-                command.CommandText = @"SELECT Id, Date, Occurance FROM drinking_water";
-
-                var reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Console.WriteLine($"{reader.GetString(0)}.\t{reader.GetString(1)}\t{reader.GetString(2)}\n");
-                }
-            }
-
-        }
-        catch (SqliteException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        Console.WriteLine("\n==================================");
-    }
-
-    // TODO: Validate Id
-    private static void UpdateHabit()
-    {
-        ViewHabits();
-
-        Console.Write("Select an id of the occurance you wish to edit: ");
-        string id = Console.ReadLine();
-
-        Console.Write("Input the updated date for the habit (yyyy-MM-dd): ");
-        string updatedDate = GetDateFromUser();
-
-        Console.Write("Input the updated number of occurances: ");
-        int updatedOccurance = GetOccuranceFromUser();
-
-        try
-        {
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-
-                var command = connection.CreateCommand();
-
-                command.CommandText = $"UPDATE drinking_water " +
-                    $"SET Date = '{updatedDate}', Occurance = '{updatedOccurance}' " +
-                    $"WHERE Id = {id}";
-
-                command.ExecuteNonQuery();
-
-                connection.Close();
-            }
-        }
-        catch (SqliteException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        Console.WriteLine("\nOccurance updated successfully!\nPress any key to continue...");
-        Console.ReadKey();
-
-        Menu();
-
     }
 
     private static void InsertHabit()
@@ -226,6 +118,123 @@ class Program
         Menu();
     }
 
+    private static void UpdateHabit()
+    {
+        ViewHabits();
+
+        Console.Write("Select an id of the occurance you wish to edit: ");
+        int id = GetIdFromUser();
+
+        Console.Write("Input the updated date for the habit (yyyy-MM-dd): ");
+        string updatedDate = GetDateFromUser();
+
+        Console.Write("Input the updated number of occurances: ");
+        int updatedOccurance = GetOccuranceFromUser();
+
+        try
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+
+                var command = connection.CreateCommand();
+
+                command.CommandText = $"UPDATE drinking_water " +
+                    $"SET Date = '{updatedDate}', Occurance = '{updatedOccurance}' " +
+                    $"WHERE Id = {id}";
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected == 0)
+                {
+                    Console.WriteLine($"No occurance found with Id {id}. Nothing was updated.\nPress any key to continue...");
+                }
+                else
+                {
+                    Console.WriteLine("\nOccurance updated successfully!\nPress any key to continue...");
+                }
+                Console.ReadKey();
+
+                connection.Close();
+            }
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static void ViewHabits()
+    {
+        Console.Clear();
+        Console.WriteLine("======WATER DRINKING HISTORY======");
+        Console.WriteLine("\nId\tDate\t\tOccurance\n");
+        try
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+
+                command.CommandText = @"SELECT Id, Date, Occurance FROM drinking_water";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetString(0)}.\t{reader.GetString(1)}\t{reader.GetString(2)}\n");
+                }
+            }
+
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("\n==================================");
+    }
+
+    private static void DeleteHabit()
+    {
+        ViewHabits();
+        Console.Write("Select an id of the occurance you wish to delete: ");
+        int id = GetIdFromUser();
+
+        try
+        {
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+
+                command.CommandText = $"DELETE FROM drinking_water WHERE Id={id}";
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if(rowsAffected == 0)
+                {
+                    Console.WriteLine($"\n\nNo occurance found with Id {id}. Nothing was deleted.\nPress any key to continue...");
+                } else
+                {
+                    Console.WriteLine("\nOccurance deleted successfully!\nPress any key to continue...");
+                }
+                Console.ReadKey();
+
+                connection.Close();
+            }
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    // Util functions
+
     static bool ValidateDate(string date)
     {
         DateTime result;
@@ -250,6 +259,23 @@ class Program
         } while (!isValid);
 
         return date;
+    }
+
+    private static int GetIdFromUser()
+    {
+        int id = -1;
+
+        do
+        {
+            id = Int32.TryParse(Console.ReadLine(), out id) ? id : -1;
+
+            if (id == -1 || id < 0)
+            {
+                Console.Write("\nInvalid Id. Please enter a valid number: ");
+            }
+        } while (id == -1 || id < 0);
+
+        return id;
     }
 
     private static int GetOccuranceFromUser()
